353a1f2f3fea33258b3a20a5aa5309dd8e1a1d2a
dax2367 <34932414+dax2367@users.noreply.github.com>
2017-12-30 08:59:25 -0800

    Push file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* Program:		Sort
+// Author:		Holly Eaton
+// Description:	Program that performs insertion, selection and bubble sorts in order, reverse order and random order.			
+//======================================================================================================================*/
+
+#include <iostream>
+#include "Sort.h"
+
+using namespace std;
+
+void main()
+{
+	//Start the sorting
+	Sort newSort;
+	newSort.runSort();
+	cin.get();
+}
